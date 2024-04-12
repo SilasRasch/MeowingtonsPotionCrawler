d2ef01f448ef9c60cdf9772659950eb3b8804b9a
Mctaowing <mikkelchristensen2001@gmail.com>
2024-04-12 13:39:36 +0200

    Lavet chest prefab og oprettet script til chests

---
+++ Assets/_Scripts/ChestScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private Sprite openSprite, closedSprite;
+
+    private bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
