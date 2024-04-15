6313f341646c567fc849a4309231d8d9ea9d245e
Silas H. Rasch <silasrasch@gmail.com>
2024-04-15 13:04:14 +0200

    oprydning i attack

--- Assets/_Scripts/AttackScript.cs
+++ Assets/_Scripts/AttackScript.cs
@@ -7,6 +7,7 @@ public class AttackScript : MonoBehaviour
     private Animator animator;
     private CapsuleCollider2D hitbox;
     private GameObject staff;
+    private Vector3 rotation;
 
     private void Start()
     {
@@ -22,17 +23,34 @@ public class AttackScript : MonoBehaviour
             //Debug.Log("Attack");
             //animator.SetTrigger("MeleeAttack");
             Invoke("ActivateHitbox", 0.2f); // Activate hitbox after 0.2 seconds.
+            
             Invoke("DeactivateHitbox", 0.4f); // Deactivate hitbox after 0.4 seconds.
         }
     }
 
     void ActivateHitbox()
     {
+        // Get Z-rotation to reset later
+        rotation = staff.transform.eulerAngles;
+
+        // set hitbox active
         hitbox.gameObject.SetActive(true);
+        //animator.SetBool("IsFlipped", staff.GetComponentInParent<PlayerMovement>().IsFlipped);
+        //animator.SetBool("IsMeleeing", true);
+
+        // Run "Animation"
+        staff.transform.eulerAngles = new Vector3(rotation.x,
+                        rotation.y,
+                        -50);
     }
 
     void DeactivateHitbox()
     {
+        // Deactivate hitbox
         hitbox.gameObject.SetActive(false);
+        //animator.SetBool("IsMeleeing", false);
+
+        // Reset staff-rotation
+        staff.transform.eulerAngles = rotation;
     }
 }
